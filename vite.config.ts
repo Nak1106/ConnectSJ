e651bce19a5447a1f5c1db5f497cf4a12d113764
Akshar Mehta <63180150+HodeX7@users.noreply.github.com>
2025-04-26 23:38:08 -0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,30 +1,21 @@
-// import { defineConfig } from "vite";
-// import react from "@vitejs/plugin-react";
-
-// export default defineConfig({
-//   plugins: [react()],
-//   server: {
-//     proxy: {
-//       "/chatapi": {
-//         target: "https://api.langflow.astra.datastax.com",
-//         changeOrigin: true,
-//         rewrite: (path) => path.replace(/^\/chatapi/, ""),
-//         secure: true,
-//       },
-//       "/api": {
-//         target: "http://localhost:3000",
-//         changeOrigin: true,
-//       },
-//     },
-//   },
-// });
-
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist',
+  server: {
+    proxy: {
+      "/chatapi": {
+        target: "https://api.langflow.astra.datastax.com",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/chatapi/, ""),
+        secure: true,
+      },
+      "/api": {
+        target: "http://localhost:3000",
+        changeOrigin: true,
+      },
+    },
   },
 });
+
